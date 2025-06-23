903941461240362b4e56a46e9b0f54c0a6b8d38d
Christian Thomas <christianmthomas@gmail.com>
2025-06-23 14:26:18 -0600

    call lineups API

--- app.py
+++ app.py
@@ -44,7 +44,15 @@ async def root():
 @app.get("/zipcode/{zipcode}")
 async def lineups_zipcode(zipcode: int):
     print(zipcode)
-    lineup = Lineups(**test_data)
+    api_url = "http://data.tmsapi.com/v1.1/lineups?country=USA&postalCode=78701&api_key=kua9569t57crx43pdan75m8v"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(api_url)
+        all_lineups = response.json()
+    if all_lineups:
+        print(all_lineups)
+    else:
+        print("No LineUps found")
+    #lineup = Lineups(**test_data)
     return lineup
 
 async def api_call():
