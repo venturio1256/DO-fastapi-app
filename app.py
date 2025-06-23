63f4a1ed3f2c192cbcc3211ca4922bcb9d04432d
Christian Thomas <christianmthomas@gmail.com>
2025-06-23 13:42:40 -0600

    Field pydantic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 """
 {
@@ -14,12 +14,12 @@ from pydantic import BaseModel
 }
 """
 class Lineups(BaseModel):
-    lineupType: str
-    lineupDevice: str
-    lineupId: str
-    lineupName: str
-    lineupLocation: str
-    lineupMso: dict[str, str] = {}
+    lineupType: str = Field(alias='type')
+    lineupDevice: str = Field(alias='device')
+    lineupId: str = Field(alias='lineupId')
+    lineupName: str = Field(alias='name')
+    lineupLocation: str = Field(alias='location')
+    lineupMso: dict[str, str] = Field(alias='mso')
 
 test_data = {
         "type": "VMVPD",
