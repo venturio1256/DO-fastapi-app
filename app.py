4d4d7daef064fa983c1e76c41e63aba83d347b97
Christian Thomas <christianmthomas@gmail.com>
2025-06-23 04:19:50 -0600

    first endpoint

--- app.py
+++ app.py
@@ -1,4 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+"""
+{
+  "type": "CABLE",
+  "device": "X",
+  "lineupId": "USA-CA66511-X",
+  "name": "AT&T U-verse TV",
+  "location": "Los Angeles",
+  "mso": {
+    "id": "17304",
+    "name": "AT&T U-verse TV"
+}
+"""
+class Lineups(BaseModel):
+    lineupType: str
+    lineupDevice: str
+    lineupId: str
+    lineupName: str
+    lineupLocation: str
+    lineupMso: dict[str, str] = {}
+
+test_data = {
+        "type": "VMVPD",
+        "device": "X",
+        "lineupId": "USA-HULU751-X",
+        "name": "Hulu Denver",
+        "location": "Denver",
+        "mso": {
+            "id": "17842",
+            "name": "Hulu"
+        }
+    }
 
 app = FastAPI()
 
@@ -6,3 +39,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to venturio Labs/v1/test "}
+
+@app.get("/zipcode/{zipcode}")
+async def lineups_zipcode(zipcode: int):
+    lineup = Lineups(**test_data)
+    return lineup
