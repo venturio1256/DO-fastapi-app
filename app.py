d3b41149b13db15bc1c5f423367f62dac1d38ebc
Christian Thomas <christianmthomas@gmail.com>
2025-06-24 15:48:34 -0600

    function api_call

--- app.py
+++ app.py
@@ -3,18 +3,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field
 import httpx
 
-"""
-{
-  "type": "CABLE",
-  "device": "X",
-  "lineupId": "USA-CA66511-X",
-  "name": "AT&T U-verse TV",
-  "location": "Los Angeles",
-  "mso": {
-    "id": "17304",
-    "name": "AT&T U-verse TV"
-}
-"""
+base_url: str = "http://data.tmsapi.com/v1.1/"
+api_key: str = "kua9569t57crx43pdan75m8v"
+
 class Lineups(BaseModel):
     lineupType: str = Field(alias='type')
     lineupDevice: str = Field(alias='device', default=None)
@@ -46,11 +37,13 @@ async def root():
 async def lineups_zipcode(zipcode: int):
     print(zipcode)
     lineUps = []
-    api_url = "http://data.tmsapi.com/v1.1/lineups?country=USA&postalCode=78701&api_key=kua9569t57crx43pdan75m8v"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(api_url)
-        all_lineups = response.json()
-    if all_lineups:
+    query_param = "lineups?country=USA&postalCode=" + str(zipcode)
+    api_response = api_call(base_url, query_param, api_key)
+    #api_url = "http://data.tmsapi.com/v1.1/lineups?country=USA&postalCode=78701&api_key=kua9569t57crx43pdan75m8v"
+    #async with httpx.AsyncClient() as client:
+    #    response = await client.get(api_url)
+    all_lineups = api_response.json()
+    if all_lineups: 
         print(all_lineups)
         for lineup in all_lineups:
             print(lineup)
@@ -61,5 +54,10 @@ async def lineups_zipcode(zipcode: int):
     #lineup = all_lineups[0]
     return lineUps
 
-async def api_call():
-    pass
+async def api_call(base_url: str, api_key: str, query_params: str):
+    q_api_key = "&api_key=" + api_key
+    api_url = base_url + query_params + q_api_key
+    #api_url = "http://data.tmsapi.com/v1.1/lineups?country=USA&postalCode=78701&api_key=kua9569t57crx43pdan75m8v"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(api_url)    
+    return response
