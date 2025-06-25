c2a9204b85c50c075775e7ce114aad5c761a7275
Christian Thomas <christianmthomas@gmail.com>
2025-06-24 20:57:47 -0600

    only passing the param by ref

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ async def lineups_zipcode(zipcode: int):
     lineUps = []
     query_param = "lineups?country=USA&postalCode=" + str(zipcode)
     print(query_param)
-    api_response = await api_call(base_url, query_param, api_key)
+    api_response = await api_call(query_param)
     #api_url = "http://data.tmsapi.com/v1.1/lineups?country=USA&postalCode=78701&api_key=kua9569t57crx43pdan75m8v"
     #async with httpx.AsyncClient() as client:
     #    response = await client.get(api_url)
@@ -55,7 +55,7 @@ async def lineups_zipcode(zipcode: int):
     #lineup = all_lineups[0]
     return lineUps
 
-async def api_call(base_url: str, api_key: str, query_params: str):
+async def api_call(query_params: str):
     q_api_key = "&api_key=" + api_key
     api_url = base_url + query_params + q_api_key
     print(api_url)
